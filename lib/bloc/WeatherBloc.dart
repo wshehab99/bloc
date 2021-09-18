a6832e98329f7e9b7efbe3c3f8a5e59bc08bff00
unknown <waleedbinshihab@gmail.com>
2021-09-18 09:54:42 +0200

    first

---
+++ lib/bloc/WeatherBloc.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:summer_iti_api_bloc/bloc/WeatherEvent.dart';
+import 'package:summer_iti_api_bloc/bloc/WeatherState.dart';
+import 'package:summer_iti_api_bloc/data/remote/api_services.dart';
+import 'package:summer_iti_api_bloc/model/base_exception.dart';
+import 'package:summer_iti_api_bloc/model/weather.dart';
+
+class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  ApiServices? _apiServices;
+
+  WeatherBloc(WeatherState initialState, this._apiServices)
+      : super(initialState);
+
+  @override
+  Stream<WeatherState> mapEventToState(WeatherEvent event) async* {
+    switch (event) {
+      case WeatherEvent.FETCH_WEATHER:
+        try {
+          WeatherResponse? weatherResponse =
+              await _apiServices!.getWeather('New York');
+          yield SuccessWeatherState(weatherResponse);
+        } catch (e) {
+          yield ErrorWeatherState(BaseException(message: e.toString()));
+        }
+    }
+  }
+}
---
+++ lib/data/Constants.dart
@@ -0,0 +1,4 @@
+class Constants{
+  static final String BASE_URL = "http://api.weatherstack.com";
+  static final String API_KEY = "93cf8f53901fbb1fd1dc26e0f11d7170";
+}
